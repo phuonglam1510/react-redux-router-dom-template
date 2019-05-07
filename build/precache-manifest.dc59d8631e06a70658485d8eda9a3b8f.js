01b0184f0948a4f34d0c21f153768e3a963803d6
Phuong Lam <phuonglam1510@gmail.com>
2019-05-07 10:48:59 +0700

    Deploy code to NOW

---
+++ build/precache-manifest.dc59d8631e06a70658485d8eda9a3b8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "07b6c36ad615a24e100c",
+    "url": "/static/js/main.72248900.chunk.js"
+  },
+  {
+    "revision": "d857a2ec5755471800e9",
+    "url": "/static/js/2.efef4777.chunk.js"
+  },
+  {
+    "revision": "07b6c36ad615a24e100c",
+    "url": "/static/css/main.d08355a8.chunk.css"
+  },
+  {
+    "revision": "d857a2ec5755471800e9",
+    "url": "/static/css/2.3afeb5d4.chunk.css"
+  },
+  {
+    "revision": "936e3bee7f692a940f87d540a6732c92",
+    "url": "/index.html"
+  }
+];
