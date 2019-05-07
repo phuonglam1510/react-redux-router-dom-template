23dc42504126ccd1af3ccd4baf88360bd1efb896
Phuong Lam <phuonglam1510@gmail.com>
2019-05-07 11:32:58 +0700

    FIx NOW

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
++app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen();
+
+console.log("Node server running....");
